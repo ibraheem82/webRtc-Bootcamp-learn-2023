64de04b27f2ed839bb4ecf389c5f04d985a4ed72
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-10-24 19:54:46 +0100

    "Initialize local video stream using getUserMedia API
    Configure the 'navigator.getUserMedia' function to request access to both video and audio. Upon successful acquisition of the user's media stream ('myStream'), set the 'local_video' element's 'srcObject' property to display the local video feed. Handle errors by logging them to the console."

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,4 +1,15 @@
 var url_string = window.location.href;
 var url = new URL(url_string);
-
 var username = url.searchParams.get('username');
+
+
+var local_video = document.querySelector("#local-video");
+navigator.getUserMedia({
+    video:true, 
+    audio:true},
+    function(myStream){
+        stream = myStream;
+        local_video.srcObject = stream;
+    }, function(error){
+        console.log(error);
+    });

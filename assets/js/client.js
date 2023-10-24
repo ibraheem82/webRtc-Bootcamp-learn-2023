deee9d577bd159851135ae2892093593121b8d92
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-10-24 20:00:12 +0100

    comments added...

--- assets/js/client.js
+++ assets/js/client.js
@@ -4,12 +4,15 @@ var username = url.searchParams.get('username');
 
 
 var local_video = document.querySelector("#local-video");
+// * To  access the user's camera and microphone.
 navigator.getUserMedia({
     video:true, 
     audio:true},
+    // * This function will be called if, if access to the user's media is successful
     function(myStream){
         stream = myStream;
         local_video.srcObject = stream;
+        // ! For error
     }, function(error){
         console.log(error);
     });

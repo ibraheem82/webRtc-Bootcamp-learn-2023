b079fdf07d926012d2b066d9a3620b4cb31f9e5b
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-10-26 20:07:56 +0100

    ℹ️ Establish WebSocket connection with signalling server
    Initialize a WebSocket connection to 'ws://localhost:9090', designating the server as the signalling server. The connection's `onopen` event logs a message indicating successful connection to the server. Additionally, implement handling of incoming messages using the `onmessage` event to parse JSON data.
    
    Handle connection errors by utilizing the `onerror` event, parsing the error message from the received JSON data, and logging it to the console.
    
    This commit enhances the reliability and error handling of the WebSocket connection, providing clear feedback during connection establishment and error scenarios.

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,3 +1,22 @@
+var connection = new WebSocket('ws://localhost:9090');
+
+connection.onopen = function(){
+    // the server is the signalling server
+    console.log("Connected to the server")
+}
+
+// after getting message from the signalling server
+connection.onmessage = function(msg){
+    var data = JSON.parse(msg.data)
+}
+
+// if there is error in connection or if the connection is not established.
+connection.onmessage = function(error){
+    var data = JSON.parse(msg.data)
+    console.log(error)
+}
+
+
 var url_string = window.location.href;
 var url = new URL(url_string);
 var username = url.searchParams.get('username');

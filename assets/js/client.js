05318335a79a2e35e970a0fba1f19f44bacd65bd
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-10-26 20:16:32 +0100

    initialized...

--- assets/js/client.js
+++ assets/js/client.js
@@ -17,7 +17,7 @@ connection.onerror = function(error){
 
 
 var name;
-
+var connectedUser;
 var url_string = window.location.href;
 var url = new URL(url_string);
 var username = url.searchParams.get('username');
